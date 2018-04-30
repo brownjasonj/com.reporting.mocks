a32b6e9a559668e71fec9c76d4a7d7692deb9a23
Jason Brown <jasonbrown@fastmail.fm>
2018-04-30 19:01:41 +0200

    refactored the risk generator.  Added functions to handle STMR and MTMR

--- src/main/java/com/reporting/mocks/generators/RiskRunGenerator.java
+++ src/main/java/com/reporting/mocks/generators/RiskRunGenerator.java
@@ -3,10 +3,9 @@ package com.reporting.mocks.generators;
 import com.reporting.mocks.configuration.PricingGroupConfig;
 import com.reporting.mocks.model.*;
 import com.reporting.mocks.model.risks.Risk;
+import com.reporting.mocks.model.risks.RiskType;
 import com.reporting.mocks.process.risks.ResultKind;
-import com.reporting.mocks.process.risks.requests.MTSRRiskRunRequest;
-import com.reporting.mocks.process.risks.requests.RiskRunRequest;
-import com.reporting.mocks.process.risks.requests.STSRRiskRunRequest;
+import com.reporting.mocks.process.risks.requests.*;
 import com.reporting.mocks.process.risks.response.MRRunResponse;
 import com.reporting.mocks.process.risks.response.RiskRunResult;
 import com.reporting.mocks.process.risks.response.SRRunResponse;
@@ -30,21 +29,56 @@ public class RiskRunGenerator {
             }
         }
 
-        int fragSize = riskRunRequest.getFragmentSize();
-        int fragCount = risks.size() / fragSize + ((risks.size() % fragSize != 0 ? 1 : 0));
-
-        for(int frag = 0; frag < risks.size(); frag += riskRunRequest.getFragmentSize()) {
-            //  public RiskRunResult(ResultKind kind, int fragmentCount, int fragmentNo, RiskRunRequest request, List<Risk> risks) {
-
-            int fragNo = frag / fragSize;
-            int indexStart = frag;
-            int indexFinish = frag + fragSize > risks.size() ? risks.size() - 1 : frag + fragSize - 1;
-            RiskRunResult rrr = new MRRunResponse(ResultKind.Fragment, fragCount, fragNo, riskRunRequest, risks.subList(frag, indexFinish));
-            riskRunResults.add(rrr);
-        }
+        riskRunResults.addAll(RiskRunGenerator.fragmentResults(risks, riskRunRequest.getFragmentSize(), riskRunRequest));
 
         return riskRunResults;
      }
+
+     protected static List<RiskRunResult> fragmentResults(List<Risk> risks, int fragSize, RiskRunRequest riskRunRequest) {
+         List<RiskRunResult> riskRunResults = new ArrayList<>();
+         int fragCount = risks.size() / fragSize + ((risks.size() % fragSize != 0 ? 1 : 0));
+
+         for (int frag = 0; frag < risks.size(); frag += fragSize) {
+             int fragNo = frag / fragSize;
+             int indexStart = frag;
+             int indexFinish = frag + fragSize > risks.size() ? risks.size() - 1 : frag + fragSize - 1;
+             RiskRunResult rrr = new MRRunResponse(ResultKind.Fragment, fragCount, fragNo, riskRunRequest, risks.subList(frag, indexFinish));
+             riskRunResults.add(rrr);
+         }
+
+         return riskRunResults;
+     }
+
+     public static List<RiskRunResult> generate(TradePopulation tradePopulation, MTMRRiskRunRequest riskRunRequest) {
+        List<RiskRunResult> riskRunResults = new ArrayList<>();
+        Collection<Trade> trades = tradePopulation.getTrades();
+        for(RiskType riskType : riskRunRequest.getRiskTypes()) {
+            List<Risk> risks = new ArrayList<>();
+            IRiskGenerator riskGenerator = RiskGeneratorFactory.getGenerator(riskType);
+            if (riskGenerator != null) {
+                for (Trade t : trades) {
+                    risks.add(riskGenerator.generate(riskRunRequest, t));
+                }
+            }
+
+            riskRunResults.addAll(RiskRunGenerator.fragmentResults(risks, riskRunRequest.getFragmentSize(), riskRunRequest));
+        }
+
+        return riskRunResults;
+    }
+
+    public static List<RiskRunResult> generate(STMRRiskRunRequest riskRunRequest) {
+        List<RiskRunResult> riskRunResults = new ArrayList<>();
+        for(RiskType riskType : riskRunRequest.getRiskTypes()) {
+            IRiskGenerator riskGenerator = RiskGeneratorFactory.getGenerator(riskType);
+            if (riskGenerator != null) {
+                Risk risk = riskGenerator.generate(riskRunRequest, riskRunRequest.getTrade());
+                riskRunResults.add(new SRRunResponse(ResultKind.Complete, riskRunRequest, risk));
+            }
+        }
+        return riskRunResults;
+
+    }
 
     public static List<RiskRunResult> generate(STSRRiskRunRequest riskRunRequest) {
         List<RiskRunResult> riskRunResults = new ArrayList<>();
